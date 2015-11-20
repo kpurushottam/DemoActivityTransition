a14e1f82a3da9c912d8369255595649c75ce184e
unknown <purushottam.kumar@user492.aepl.com>
2015-11-20 15:52:54 +0530

    Addition: Added circular Hide-Show transition animation to view

--- app/src/main/java/com/krp/android/demoactivitytransition/SecondActivity.java
+++ app/src/main/java/com/krp/android/demoactivitytransition/SecondActivity.java
@@ -76,4 +76,61 @@ public class SecondActivity extends AppCompatActivity {
             anim.start();
         }
     }
+
+    //
+    public void search(View btnView) {
+        View toggleView = findViewById(R.id.tv_search);
+
+        // get the center for the clipping circle
+        int cx = toggleView.getRight() - btnView.getHeight()/2;
+        int cy = toggleView.getTop() - toggleView.getHeight() + btnView.getHeight()/2;
+
+        // get the final radius for the clipping circle
+        int finalRadius = Math.max(toggleView.getWidth(), toggleView.getHeight());
+
+        // get the initial radius for the clipping circle
+        int initialRadius = btnView.getWidth();
+
+        if(toggleView.getVisibility() == View.VISIBLE) {
+            hide(toggleView, btnView, cx, cy, initialRadius);
+        } else {
+            reveal(toggleView, btnView, cx, cy, finalRadius);
+        }
+    }
+
+    private void reveal(View toggleView, View btnView, int cx, int cy, int finalRadius) {
+        // create the animator for this view (the start radius is zero)
+        Animator anim = null;
+        if(Build.VERSION.SDK_INT == Build.VERSION_CODES.LOLLIPOP) {
+            anim = ViewAnimationUtils.createCircularReveal(toggleView, cx, cy, 0, finalRadius);
+        }
+
+        // make the view visible and start the animation
+        toggleView.setVisibility(View.VISIBLE);
+        if(anim != null) {
+            anim.start();
+        }
+    }
+
+    private void hide(final View toggleView, View btnView, int cx, int cy, int initialRadius) {
+        // create the animation (the final radius is zero)
+        Animator anim = null;
+        if(Build.VERSION.SDK_INT == Build.VERSION_CODES.LOLLIPOP) {
+            anim = ViewAnimationUtils.createCircularReveal(toggleView, cx, cy, initialRadius, 0);
+        }
+
+        // make the view invisible when the animation is done
+        anim.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                super.onAnimationEnd(animation);
+                toggleView.setVisibility(View.GONE);
+            }
+        });
+
+        // start the animation
+        if(anim != null) {
+            anim.start();
+        }
+    }
 }
